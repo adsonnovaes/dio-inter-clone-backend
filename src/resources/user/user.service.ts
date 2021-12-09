09d5a22c083fc0bf81ca4db31964215f1fd2bd90
Adson Novaes <adsonnovaes7@gmail.com>
2021-12-09 03:03:27 -0300

    Jwt: user access token

--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -1,6 +1,9 @@
 import { getRepository } from 'typeorm';
 import md5 from 'crypto-js/md5';
+import { sign } from 'jsonwebtoken';
 import { User } from '../../entity/User';
+
+import authConfig from '../../config/auth';
 
 import { UserSignIn } from './dtos/user.signin.dtos';
 import { UserSignUp } from './dtos/user.signup.dtos';
@@ -24,7 +27,23 @@ export default class UserService {
       throw new AppError('Usuário não encontrado', 401);
     }
 
-    return existUser;
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({
+      firstName: existUser.firstName,
+      lastName: existUser.lastName,
+      accountNumber: existUser.accountNumber,
+      accountDigit: existUser.accountDigit,
+      wallet: existUser.wallet
+    }, secret, {
+      subject: existUser.id,
+      expiresIn,
+    });
+
+    // @ts-expect-error ignora
+    delete existUser.password
+
+    return { accessToken: token }
   }
 
   async signup(user: UserSignUp) { }
